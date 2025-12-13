f8965af54144c71a3f2bc02b3c95ba69485cd1e8
HappyRehund <rayhanardian045@gmail.com>
2025-12-13 21:44:24 +0700

    init database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,26 @@ import { UserModule } from './user/user.module';
 import { TravelSpotModule } from './travel-spot/travel-spot.module';
 import { TravelPackageModule } from './travel-package/travel-package.module';
 import { TravelTripModule } from './travel-trip/travel-trip.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/database.config';
 
 @Module({
-  imports: [AuthModule, UserModule, TravelSpotModule, TravelPackageModule, TravelTripModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => typeOrmConfig(configService)
+    }),
+    AuthModule,
+    UserModule,
+    TravelSpotModule,
+    TravelPackageModule,
+    TravelTripModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
